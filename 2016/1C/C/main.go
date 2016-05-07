5667031facf9b295f5cb62bea072c59ad990fabb
rcarrier <rcarrier@byteback.com.au>
2016-05-07 21:55:33 +1000

    added new template and template for 1C

---
+++ 2016/1C/C/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var tests, n int
+
+var line string
+
+func main() {
+	fmt.Scan(&tests)
+	for test := 1; test <= tests; test++ {
+		fmt.Printf("Case #%d: ", test)
+		solve()
+		fmt.Println()
+	}
+}
+
+func solve() {
+
+}
