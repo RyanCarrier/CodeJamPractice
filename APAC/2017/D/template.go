0ae8aaf71bd72a4b2a92d2bf8a3b97ba971f9f60
rcarrier <rcarrier@byteback.com.au>
2016-07-10 13:17:51 +1000

    init APC

---
+++ APAC/2017/D/template.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+//T test cases
+var T int
+
+var line string
+
+func main() {
+	fmt.Scan(&T)
+	for test := 1; test <= T; test++ {
+		fmt.Printf("Case #%d: ", test)
+		solve()
+	}
+}
+
+func solve() {
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CodeJamPractice
+My repo for code jam practice problems, and just code jam comps in general.
+
+All solutions are most likely written in golang.
+
+I might have some Java or Python in there somewhere.
